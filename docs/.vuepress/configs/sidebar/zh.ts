4bebbbf6cc63250776588a1c2ae978a72593431f
rich1e <yuqigong@outlook.com>
2023-03-09 15:58:59 +0800

    feat: modify sidebar

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -2,7 +2,7 @@
  * @Author       : yuqigong@outlook.com
  * @Date         : 2023-02-02 09:36:51
  * @LastEditors  : yuqigong@outlook.com
- * @LastEditTime : 2023-03-06 18:42:54
+ * @LastEditTime : 2023-03-09 15:19:51
  * @FilePath     : /monorepo-starter/docs/.vuepress/configs/sidebar/zh.ts
  * @Description  :
  *
@@ -12,22 +12,8 @@ import type { SidebarConfig } from 'vuepress';
 export const zh: SidebarConfig = {
   '/components': [
     {
-      text: 'Button 按钮',
-      children: [
-        {
-          text: 'Button 按钮',
-          link: '/components/button/',
-        },
-      ],
-    },
-    {
-      text: 'Switch 开关',
-      children: [
-        {
-          text: 'Switch 开关',
-          link: '/components/switch/',
-        },
-      ],
+      text: 'Components',
+      children: ['/components/button', '/components/switch'],
     },
   ],
 };
--- packages/components/switch/README.md
+++ packages/components/switch/README.md
@@ -6,7 +6,7 @@ permalink: /components/switch
 
 表示两种相互对立的状态间的切换，多用于触发「开/关」。
 
-## 基础用法
+基础用法
 
 :::demo
 switch/base

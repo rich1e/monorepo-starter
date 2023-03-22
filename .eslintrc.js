d596059de76d51b7368ec6e872354a9d0726ef02
rich1e <yuqigong@outlook.com>
2023-03-22 14:18:33 +0800

    fix: fix typescript error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author       : yuqigong@outlook.com
  * @Date         : 2023-03-03 15:29:21
  * @LastEditors  : yuqigong@outlook.com
- * @LastEditTime : 2023-03-09 18:34:59
+ * @LastEditTime : 2023-03-22 14:15:49
  * @FilePath     : /monorepo-starter/.eslintrc.js
  * @Description  :
  *
@@ -15,9 +15,14 @@ module.exports = {
      * Error while loading rule '@typescript-eslint/dot-notation': You have used a rule which requires parserServices
      * @see https://stackoverflow.com/questions/64116378/error-while-loading-rule-typescript-eslint-dot-notation
      * @see https://typescript-eslint.io/linting/typed-linting/monorepos#one-tsconfigjson-per-package-and-an-optional-one-in-the-root
+     * @see https://github.com/typescript-eslint/typescript-eslint/blob/main/.eslintrc.js
+     * @see https://github.com/typescript-eslint/typescript-eslint/blob/main/tsconfig.eslint.json
      */
-    // project: ['tsconfig.json', './apps/*/tsconfig.json'],
-    project: ['tsconfig.json', './**/tsconfig.json'],
+    project: [
+      'tsconfig.json',
+      './apps/*/tsconfig.json',
+      './packages/*/tsconfig.json',
+    ],
     extraFileExtensions: ['.vue'],
   },
   extends: '@firmiana/eslint-config',
